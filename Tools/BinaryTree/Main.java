40e559a4afc360541cce131cc9e4aad581b05ca7
Ancient Mariner <tomweisenbeckmacbook@gmail.com>
2018-07-09 07:05:27 -0400

    init commit

---
+++ Tools/BinaryTree/Main.java
@@ -0,0 +1,25 @@
+package BinaryTree;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Node aNode = new Node('A');
+    Node cNode = new Node('C');
+    Node mNode = new Node('M');
+    Node pNode = new Node('P');
+    Node zNode = new Node('Z');
+    BinaryTree tree = new BinaryTree(mNode);
+
+    tree.insert(tree.getRoot(), aNode);
+    tree.insert(tree.getRoot(), zNode);
+
+    tree.insert(tree.getRoot(), cNode);
+    tree.insert(tree.getRoot(), pNode);
+
+    System.out.println("Root node: " + tree.getRoot().getData());
+    System.out.println("Root's Left: " + tree.getRoot().left.getData());
+    System.out.println("Root's Right: " + tree.getRoot().right.getData());
+
+    System.out.println("Search for A: " + tree.search('A'));
+  }
+}
